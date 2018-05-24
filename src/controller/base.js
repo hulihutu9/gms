c5b8419e782982cb4b32e302bd8823553c7a7f34
peter <huozhiniao@sina.com>
2018-05-25 07:23:45 +0800

    增加登陆功能

--- src/controller/base.js
+++ src/controller/base.js
@@ -1,5 +1,17 @@
 module.exports = class extends think.Controller {
-  __before() {
+  async __before() {
+    // 如果是登陆页面（user_login），直接返回。
+    if (this.ctx.controller === 'user' && this.ctx.action === 'login') {
+      return;
+    }
 
+    // 获取session中的用户信息
+    const userinfo = await this.session('userinfo');
+    // 用户信息不为空，继续
+    if (!think.isEmpty(userinfo)) {
+      this.assign('userinfo', userinfo);
+    } else { // 用户信息为空，跳转到登陆页面
+      return this.redirect('/user/login');
+    }
   }
 };
--- src/controller/user.js
+++ src/controller/user.js
@@ -1,5 +1,27 @@
-const BaseRest = require('./rest.js');
+const Base = require('./base.js');
+const md5 = require('js-md5');
 
-module.exports = class extends BaseRest {
+module.exports = class extends Base {
+  indexAction() {
+    return this.display();
+  }
 
+  async loginAction() {
+    // 如果是POST过来登录信息
+    if (this.isPost) {
+      const name = this.post('name');
+      const password = md5(this.post('password'));
+
+      const data = await this.model('user').where({ name: name, password: password }).find();
+      // 用户名、密码不正确，返回错误信息
+      if (think.isEmpty(data)) {
+        this.assign('error', '用户名或密码错误');
+        return this.display();
+      } else { // 用户名、密码正确，设置session，跳转到index页面
+        await this.session('userinfo', data);
+        return this.redirect('/index/index');
+      }
+    }
+    return this.display();
+  }
 };
